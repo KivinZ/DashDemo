20c5056034a85361c8d1aaa1ede9e01df06cb1b5
KivinZ <48145327+KivinZ@users.noreply.github.com>
2019-03-02 02:41:12 -0500

    Add files via upload

---
+++ dash_starter_code.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 21:48:11 2019
+
+@author: Kevin Zhang
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 10:59:57 2019
+
+@author: Kevin Zhang
+"""
+
+import dash
+import pandas as pd
+
+from dash.dependencies import Output, Input
+from dash.exceptions import PreventUpdate
+
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.graph_objs as go
+
+
+    
+if __name__ == '__main__':
+    app.run_server(debug=True)
